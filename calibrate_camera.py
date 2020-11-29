2b1d5b2e8087cdfbb55367b5ef89d8c04974b259
nmiyamoto <scotsdowneroad@gmail.com>
2020-11-29 15:20:09 +0900

    add calibration scripts

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -2,12 +2,58 @@ import cv2
 import numpy as np
 import glob
 
-images = files =glob.glob("./calib_images")
+square_size = 2.2      # rect size [cm]
+pattern_size = (10, 7)
+
+pattern_points = np.zeros( (np.prod(pattern_size), 3), np.float32 )
+pattern_points[:,:2] = np.indices(pattern_size).T.reshape(-1, 2)
+pattern_points *= square_size
+objpoints = []
+imgpoints = []
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# path of calibration images
+#images = files =glob.glob("./calib_images_left/*.jpg")
+images = files =glob.glob("./calib_images_right/*.jpg")
 
 def main():
+    # extract corners from chess board images
     for image in images:
-    bgr = cv2.imread(fname, cv2.IMREAD_COLOR)
-    #TODO
+        # show filename 
+        print(image)
+
+        # load images
+        bgr = cv2.imread(image, cv2.IMREAD_COLOR)
+        gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
+
+        # find corner from chessboard
+        ret, corners = cv2.findChessboardCorners(gray, pattern_size, None)
+
+        if ret == True:
+            print("corner detected!!!")
+            corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+
+            # Draw and display the corners
+            img = cv2.drawChessboardCorners(bgr, pattern_size, corners2,ret)
+            cv2.imshow('img',img)
+            cv2.waitKey(0)
+
+            imgpoints.append(corners.reshape(-1, 2))
+            objpoints.append(pattern_points)
+
+    # calibrate camera parameters
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    # save camera parameters
+    np.save("mtx", mtx)
+    np.save("dist", dist.ravel())
+
+    # show parameters
+    print("RMS = ", ret)
+    print("mtx = \n", mtx)
+    print("dist = ", dist.ravel())
+
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
